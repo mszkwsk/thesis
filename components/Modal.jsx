f37a6ded59ceaf2c4e61bb9a55aebbb8060dda34
mszkwsk <bartekmieszkowski@gmail.com>
2019-03-02 18:05:19 +0100

    Prepare simple modal

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 import '../css/modal.css'
 
 class Modal extends React.Component {
-  
+  render() {
+    const {handleClose: close} = this.props
+    return (
+      <div
+        className='modal-dim'
+        onClick={() => close()}
+      >
+        <div className = 'modal-dialog'>
+        
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Modal
